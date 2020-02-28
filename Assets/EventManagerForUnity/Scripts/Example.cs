9cb85886becfae9c01db92970b3c317d22869bd5
Kevin Castejon <pitikev1@hotmail.com>
2020-02-28 18:25:20 +0100

    Initial commit

---
+++ Assets/EventManagerForUnity/Scripts/Example.cs
@@ -0,0 +1,31 @@
+﻿using kevincastejon;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Event = kevincastejon.Event;
+
+public class Example : MonoBehaviour
+{
+    public EventDispatcherMono ed;
+    public bool dispatched = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void MyCallback(Event e)
+    {
+        Debug.Log(e.Name + " event has been dispatched");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!dispatched)
+        {
+            ed.DispatchEvent(new Event("someEventName"));
+            dispatched = true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# eventmanager-for-unity
+ EventManager package encapsulates the delegate system into an event flow similar to the Javascript event system.
