e5afdad22697baa9b96c9a290a5625191388a757
Kevin Castejon <pitikev1@hotmail.com>
2020-02-28 23:05:32 +0100

    fix namespaces

--- Assets/EventManagerForUnity/Scripts/Example.cs
+++ Assets/EventManagerForUnity/Scripts/Example.cs
@@ -1,13 +1,11 @@
-﻿using kevincastejon;
-using System.Collections;
-using System.Collections.Generic;
+﻿using kevincastejon.unity;
 using UnityEngine;
 using Event = kevincastejon.Event;
 
 public class Example : MonoBehaviour
 {
-    public EventDispatcherMono ed;
-    public bool dispatched = false;
+    public EventDispatcher Dispatcher;
+    private bool Dispatched = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +20,10 @@ public class Example : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!dispatched)
+        if (!Dispatched)
         {
-            ed.DispatchEvent(new Event("someEventName"));
-            dispatched = true;
+            Dispatcher.DispatchEvent(new Event("someEventName"));
+            Dispatched = true;
         }
     }
 }
