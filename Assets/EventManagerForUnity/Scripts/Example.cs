1b1ad96f1ae55ec26e1c42c9f8c2a02e22df375a
Kevin Castejon <pitikev1@hotmail.com>
2020-02-28 23:35:06 +0100

    Update Example.cs

--- Assets/EventManagerForUnity/Scripts/Example.cs
+++ Assets/EventManagerForUnity/Scripts/Example.cs
@@ -2,28 +2,31 @@
 using UnityEngine;
 using Event = kevincastejon.Event;
 
-public class Example : MonoBehaviour
+namespace EventManagerExample
 {
-    public EventDispatcher Dispatcher;
-    private bool Dispatched = false;
-    // Start is called before the first frame update
-    void Start()
+    public class Example : MonoBehaviour
     {
-
-    }
-
-    public void MyCallback(Event e)
-    {
-        Debug.Log(e.Name + " event has been dispatched");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!Dispatched)
+        public EventDispatcher Dispatcher;
+        private bool Dispatched = false;
+        // Start is called before the first frame update
+        void Start()
         {
-            Dispatcher.DispatchEvent(new Event("someEventName"));
-            Dispatched = true;
+
+        }
+
+        public void MyCallback(Event e)
+        {
+            Debug.Log(e.Name + " event has been dispatched");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!Dispatched)
+            {
+                Dispatcher.DispatchEvent(new Event("someEventName"));
+                Dispatched = true;
+            }
         }
     }
 }
